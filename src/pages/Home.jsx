9d1a92378f1a07d779be6b02058151641472f7df
Jean Ferreira <jeaferr22@gmail.com>
2022-11-11 00:31:21 -0300

    feat: created login google

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,68 @@
-import { useNavigate } from "react-router-dom";
-import "../sass/_home.scss"
+import "../sass/_home.scss";
 
 export function Home() {
-
   return (
     <div className="home-conteiner">
       <h1>Sobre a D'ONG</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sed est non odio efficitur mollis vitae quis tellus. Vestibulum non nulla at mauris varius luctus. Nulla dignissim eu elit ut condimentum. Ut eget dignissim libero. Nulla sed sagittis urna. Sed sit amet ante condimentum, tempus est sed, porttitor risus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Integer accumsan arcu eu lorem ullamcorper tincidunt. In velit mi, sodales quis orci in, tristique ullamcorper nibh. In lacinia eget velit eu egestas. Sed placerat turpis neque, sit amet aliquam sapien varius quis. Aliquam lacinia convallis odio a accumsan. Nunc nunc urna, consequat elementum fringilla eu, vestibulum a urna. Sed sit amet neque eget tellus eleifend egestas eu in leo. Vestibulum arcu massa, viverra ac tempus eu, suscipit sed erat.</p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sed est
+        non odio efficitur mollis vitae quis tellus. Vestibulum non nulla at
+        mauris varius luctus. Nulla dignissim eu elit ut condimentum. Ut eget
+        dignissim libero. Nulla sed sagittis urna. Sed sit amet ante
+        condimentum, tempus est sed, porttitor risus. Orci varius natoque
+        penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+        Integer accumsan arcu eu lorem ullamcorper tincidunt. In velit mi,
+        sodales quis orci in, tristique ullamcorper nibh. In lacinia eget velit
+        eu egestas. Sed placerat turpis neque, sit amet aliquam sapien varius
+        quis. Aliquam lacinia convallis odio a accumsan. Nunc nunc urna,
+        consequat elementum fringilla eu, vestibulum a urna. Sed sit amet neque
+        eget tellus eleifend egestas eu in leo. Vestibulum arcu massa, viverra
+        ac tempus eu, suscipit sed erat.
+      </p>
 
-      <p>Nullam leo dolor, iaculis eu molestie eu, vehicula et eros. In at dolor ut lorem accumsan euismod. Fusce at massa quis nisl varius suscipit. Morbi ut placerat felis. Donec sed risus orci. Integer bibendum ipsum tristique metus bibendum rhoncus. Donec consectetur lectus sed libero interdum, a ultrices orci egestas. Duis tempor magna sit amet dolor mattis, et rutrum augue pulvinar. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam quis mi lorem. Donec congue velit vitae ante interdum venenatis. Nulla ac vulputate ligula, non tristique turpis. Pellentesque semper dolor a risus sodales tempus. Quisque finibus risus in ligula elementum scelerisque. Aliquam ut lacus non massa molestie commodo.</p>
+      <p>
+        Nullam leo dolor, iaculis eu molestie eu, vehicula et eros. In at dolor
+        ut lorem accumsan euismod. Fusce at massa quis nisl varius suscipit.
+        Morbi ut placerat felis. Donec sed risus orci. Integer bibendum ipsum
+        tristique metus bibendum rhoncus. Donec consectetur lectus sed libero
+        interdum, a ultrices orci egestas. Duis tempor magna sit amet dolor
+        mattis, et rutrum augue pulvinar. Orci varius natoque penatibus et
+        magnis dis parturient montes, nascetur ridiculus mus. Nam quis mi lorem.
+        Donec congue velit vitae ante interdum venenatis. Nulla ac vulputate
+        ligula, non tristique turpis. Pellentesque semper dolor a risus sodales
+        tempus. Quisque finibus risus in ligula elementum scelerisque. Aliquam
+        ut lacus non massa molestie commodo.
+      </p>
 
-      <p>Aliquam ac ipsum gravida, pharetra mauris sed, auctor lacus. Vestibulum vel purus sit amet lectus malesuada tempus. Ut nec odio vel tellus sollicitudin lacinia. Nullam volutpat sit amet sem eu eleifend. Proin porttitor aliquam euismod. In et erat ac augue luctus efficitur quis eget erat. Mauris et felis nec nulla vulputate mattis. Nam sit amet dolor ac quam venenatis congue. Nunc in gravida enim, id viverra orci. Ut fermentum rutrum sem, at finibus quam ultrices in. Cras neque tellus, pellentesque commodo commodo quis, finibus a sem. Aenean pulvinar sed quam sed efficitur. Phasellus fermentum odio mattis orci pulvinar, in vestibulum ex pharetra. Proin interdum augue et ex mattis auctor.</p>
+      <p>
+        Aliquam ac ipsum gravida, pharetra mauris sed, auctor lacus. Vestibulum
+        vel purus sit amet lectus malesuada tempus. Ut nec odio vel tellus
+        sollicitudin lacinia. Nullam volutpat sit amet sem eu eleifend. Proin
+        porttitor aliquam euismod. In et erat ac augue luctus efficitur quis
+        eget erat. Mauris et felis nec nulla vulputate mattis. Nam sit amet
+        dolor ac quam venenatis congue. Nunc in gravida enim, id viverra orci.
+        Ut fermentum rutrum sem, at finibus quam ultrices in. Cras neque tellus,
+        pellentesque commodo commodo quis, finibus a sem. Aenean pulvinar sed
+        quam sed efficitur. Phasellus fermentum odio mattis orci pulvinar, in
+        vestibulum ex pharetra. Proin interdum augue et ex mattis auctor.
+      </p>
 
-      <p>Integer blandit dolor sit amet nibh placerat iaculis. Suspendisse blandit tristique sem, eget sodales lacus laoreet ut. Duis elementum lorem eget sem eleifend viverra. Phasellus at turpis sit amet ligula bibendum elementum. Etiam sit amet turpis orci. Curabitur aliquam efficitur ex. Mauris vel erat viverra urna lacinia porta.</p>
+      <p>
+        Integer blandit dolor sit amet nibh placerat iaculis. Suspendisse
+        blandit tristique sem, eget sodales lacus laoreet ut. Duis elementum
+        lorem eget sem eleifend viverra. Phasellus at turpis sit amet ligula
+        bibendum elementum. Etiam sit amet turpis orci. Curabitur aliquam
+        efficitur ex. Mauris vel erat viverra urna lacinia porta.
+      </p>
 
-      <p>Fusce consectetur est at auctor tincidunt. Sed sollicitudin ligula sit amet nunc porttitor, non suscipit nunc dictum. Vivamus congue et mauris ac tristique. Praesent egestas, lorem in faucibus maximus, augue ante porta nibh, a dapibus tellus dui in dolor. Aenean mauris ex, vestibulum sed ultricies vel, sagittis id tellus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed rutrum ultricies posuere.</p>
+      <p>
+        Fusce consectetur est at auctor tincidunt. Sed sollicitudin ligula sit
+        amet nunc porttitor, non suscipit nunc dictum. Vivamus congue et mauris
+        ac tristique. Praesent egestas, lorem in faucibus maximus, augue ante
+        porta nibh, a dapibus tellus dui in dolor. Aenean mauris ex, vestibulum
+        sed ultricies vel, sagittis id tellus. Lorem ipsum dolor sit amet,
+        consectetur adipiscing elit. Sed rutrum ultricies posuere.
+      </p>
     </div>
   );
 }
