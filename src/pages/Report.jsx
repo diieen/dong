472bce69341da8ad5113745ac89fce8d8e0952e9
Jean Ferreira <jeaferr22@gmail.com>
2022-11-13 17:28:13 -0300

    feat: adjustment report page

--- src/pages/Report.jsx
+++ src/pages/Report.jsx
@@ -18,33 +18,32 @@ export function Report() {
     reader.readAsDataURL(e);
   }
 
-  const geolocation = navigator.geolocation.watchPosition(
-    (position => {
-      if (position.coords.accuracy <= 20) {
+  const getLocation = () => {
+    navigator.geolocation.getCurrentPosition(
+      (position => {
         fetch(`https://nominatim.openstreetmap.org/reverse?format=json&lat=${position.coords.latitude}&lon=${position.coords.longitude}&zoom=18&addressdetails=1`)
           .then(res => res.json())
           .then(data => {
-            alert(`${data.address.road}, ${data.address.suburb} - ${data.address.city}`);
-            navigator.geolocation.clearWatch(geolocation);
+            console.log(data)
+            setAddress(data.address)
           })
-      }
-    })
-  );
-
-  // if (cords.accuracy <= 20) {
-  //   navigator.geolocation.clearWatch(geolocation);
-
-  //   setAddress('foi')
-  // }
+      })
+    );
+  }
 
   return (
-    <div>
+    <div className="ong-main">
       <BreadCrumb items={url} />
       <h1>Reportar abandono</h1>
       <p>Encontrou um animalzinho abandonado ? </p>
       <p>Onde ele se encontra?</p>
-      <p>(Caso esteja no local, permitir o navegador acessar sua localização.)</p>
-      <textarea name="" id="" cols="30" rows="10" placeholder="Descreva o local o mais detalhado possivel (Como por exemplo: Estado; Cidade; Rua; CEP.)"></textarea>
+      <button onClick={getLocation}>Usar minha localização atual</button>
+      <input type="text" className="input register-address" name="street" placeholder='Rua' required value={address.road || ""} />
+      <input type="text" className="input register-number" name="number" placeholder='Número' required />
+      <input type="text" className="input register-bairro" name="district" placeholder='Bairro' required value={address.suburb || ""} />
+      <input type="text" className="input register-city" name="city" placeholder='Cidade' required value={address.city || ""} />
+      <input type="text" className="input register-state" name="state" placeholder='Estado' required value={address.state || ""} />
+      <textarea name="" id="" cols="30" rows="10" placeholder="Descrição do local ou observações"></textarea>
       <p>Caracteristcas do animal:</p>
       <textarea name="" id="" cols="30" rows="10" placeholder="Descreva o animal o mais detalhado possivel (Como por exemplo: Raça; Cor; estatura; etc.)"></textarea>
       <p>Tem uma imagem do local ou do animal?</p>
